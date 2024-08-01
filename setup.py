ee5c41791a77efd655973d63f3db352e94b4a56b
Caleb Weinreb <calebsw@gmail.com>
2024-08-01 12:50:29 -0400

    changed package name and refatored install + added versioneer

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup, find_packages
+import setuptools
+import versioneer
 
-setup(
-    name='kpsn',
-    version='0.0.1',
-    packages=find_packages(include=["kpsn", 'kpsn.*']),
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="shape-norm",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
